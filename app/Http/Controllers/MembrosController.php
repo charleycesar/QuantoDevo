310c4ab978acae4219d70fdf880074e65824472b
Charley Oliveira <charleycesar@gmail.com>
2017-01-27 18:20:51 -0200

    First Deploy with laravel

---
+++ app/Http/Controllers/MembrosController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MembrosController extends Controller
+{
+	public function index(){
+		return view('membrosIndex')->with('nome','FullStack');
+	}
+}
